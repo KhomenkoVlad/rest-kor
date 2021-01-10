155c56f5681e36a446a443183bb5fec511181ee1
Khomenko Vlad <zatmenie8@gmail.com>
2021-01-10 23:12:37 +0200

    add main-about section

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,11 +1,13 @@
 import { Background } from './background/background';
 import { QuickMenu } from './quickMenu/quickMenu';
+import { MainAbout } from './mainAbout/mainAbout';
 
 export const Main = () =>
   <>
     <main className='layer-main'>
       <Background />
       <section className='layer content'>
+        <MainAbout />
         <QuickMenu />
       </section>
     </main>
