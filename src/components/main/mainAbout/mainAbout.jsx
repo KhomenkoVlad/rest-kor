7e072f61c605391983016784616451d3db61c09b
Khomenko Vlad <zatmenie8@gmail.com>
2021-01-12 12:42:29 +0200

    some modifications

--- src/components/main/mainAbout/mainAbout.jsx
+++ src/components/main/mainAbout/mainAbout.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 export const MainAbout = () =>
-  <article className='main-about'>
+  <article className='orange-background main-about'>
     <h2 className='main-about__caption'>О Нас</h2>
     <div className='main-about__description'>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pharetra dui tristique, viverra turpis vitae, tempus sapien. Sed non tortor molestie, vulputate odio ut, mollis leo. Curabitur a viverra felis. Etiam nec faucibus lacus, eu scelerisque ex. Curabitur suscipit augue vitae libero interdum, non egestas lectus suscipit. Vestibulum porttitor ex vel lacus venenatis, eget feugiat augue dignissim. Sed ligula nulla, convallis nec lectus eu, iaculis lobortis erat. Fusce rutrum, nisi sit amet dignissim euismod, arcu ante lobortis neque, sit amet vehicula ligula orci ut justo. In mollis feugiat nisl, eu ornare lacus euismod eu. In id risus convallis, vehicula arcu ut, imperdiet leo. Vestibulum non pharetra purus. Pellentesque non interdum massa. Etiam ligula tellus, suscipit sit amet auctor quis, bibendum at est. Phasellus gravida erat quis dolor feugiat imperdiet. Nulla magna enim, condimentum at eleifend et, ullamcorper nec risus. Vestibulum quis sagittis urna.</p>
