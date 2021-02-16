ccb7fba4249856c23cc9b9c212a6218cf6aabf2e
Khomenko Vlad <zatmenie8@gmail.com>
2021-02-16 19:46:24 +0200

    add language changing

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,11 +4,17 @@ export const Header = () =>
   <header className="header">
     <nav>
       <ul className="navigation">
-        <li className="navigation__item"><NavLink exact to='/' className='link navigation__link' activeClassName="navigation__link_active">ГЛАВНАЯ</NavLink></li>
-        <li className="navigation__item"><NavLink to='/menu' className='link navigation__link' activeClassName="navigation__link_active">МЕНЮ</NavLink></li>
-        <li className="navigation__item"><NavLink to='/about' className='link navigation__link' activeClassName="navigation__link_active">О НАС</NavLink></li>
-        <li className="navigation__item"><NavLink to='/contact' className='link navigation__link' activeClassName="navigation__link_active">КОНТАКТЫ</NavLink></li>
-        <li className="link navigation__link navigation__item-lang">UA</li>
+        <li className="navigation__item"><NavLink exact to='/' className='link navigation__link' activeClassName="navigation__link_active">Главная</NavLink></li>
+        <li className="navigation__item"><NavLink to='/menu' className='link navigation__link' activeClassName="navigation__link_active">Меню</NavLink></li>
+        <li className="navigation__item"><NavLink to='/about' className='link navigation__link' activeClassName="navigation__link_active">О Нас</NavLink></li>
+        <li className="navigation__item"><NavLink to='/contact' className='link navigation__link' activeClassName="navigation__link_active">Контакты</NavLink></li>
+        <li className="navigation-lang">
+          <div className="link navigation__link navigation-lang__item_active">UA</div>
+          <ul className="navigation-lang-list">
+            <li className="link navigation-lang__item">RU</li>
+            <li className="link navigation-lang__item">EN</li>
+          </ul>
+        </li>
       </ul>
     </nav>
   </header>
